93f5a66986576eaa040159a23c5901c9a633ebbb
reyhannaufalh <91453451+reyhannaufalh@users.noreply.github.com>
2022-11-03 17:38:30 +0700

    Update Anggota4

--- app/src/main/java/com/example/rpl/Anggota4.java
+++ app/src/main/java/com/example/rpl/Anggota4.java
@@ -9,10 +9,21 @@ import android.widget.Button;
 
 public class Anggota4 extends AppCompatActivity {
 
+    Button kembali;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anggota4);
 
+        kembali = (Button) findViewById(R.id.kembali4);
+
+        kembali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intReg = new Intent(getApplicationContext(), ListAnggota.class);
+                startActivity(intReg);
+            }
+        });
     }
 }
